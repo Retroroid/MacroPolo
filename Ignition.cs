8811071ed20106a19085e03ea1c3686127d1cdf0
Retroroid <51481375+Retroroid@users.noreply.github.com>
2019-06-11 14:37:49 -0400

    Add files via upload

---
+++ Ignition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace MacroPolo {
+    class Ignition {
+        public static void Ignite(string path) { // Start a program from the specified filepath, path (.exe, or similar)
+            try {
+                using (Process pro = new Process()) {
+                    pro.StartInfo.UseShellExecute = false;
+                    pro.StartInfo.FileName = path;
+                    pro.Start();
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
