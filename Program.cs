889c83352928ba22c88d39ea6559760ee1b76bd4
Retroroid <51481375+Retroroid@users.noreply.github.com>
2019-06-26 10:18:42 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,17 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using WindowsInput;
 
 // ---------------------------------------------------------------- //
 namespace MacroPolo {
     class Program {
         static void Main(string[] args) {
+            InputSimulator Kami = new InputSimulator();
+
             // string m = @"C:\\Users\\lucask\\testmat\\";
             //string path = @"C:\\Windows\\System32\\mspaint.exe";
             //Ignition.Ignite(path);
             // ---------------------------------------------------------------- //
+            //Thread.Sleep(5000);
+            //VirtualBoard.Send(VirtualBoard.ScanCodeShort.F5);
             Thread.Sleep(5000);
-            VirtualBoard.Send(VirtualBoard.ScanCodeShort.F5);
+            Kami.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F5);
         }
     }
 }
